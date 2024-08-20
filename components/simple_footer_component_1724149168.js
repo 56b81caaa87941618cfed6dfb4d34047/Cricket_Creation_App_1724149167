7b807d9c7589303bc09135c4dcc6c7cfdb53c051
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-20 10:20:06 +0000

    Merge code from frontend

--- components/simple_footer_component_1724149168.js
+++ components/simple_footer_component_1724149168.js
@@ -2,13 +2,13 @@
 */
 Vue.component("simple_footer_component_1724149168", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+    <footer id="footer-section" style="min-height: 350px" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
             <div id="footer-container" class="max-w-screen-xl p-6 py-8 mx-auto lg:py-16 md:p-10 lg:p-12">
                 <hr id="footer-divider" class="my-6 border-pink-300 sm:mx-auto lg:my-8">
                 <div id="footer-content" class="text-center">
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-white hover:text-pink-200 transition-colors duration-300">
-                            <img id="footer-logo" src="./images/logo.svg" class="h-8 mr-3 sm:h-10" alt="Landwind Logo" />
+                            <img id="footer-logo" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/Cricket_Creation_App_1724149167/logo.svg" class="h-8 mr-3 sm:h-10" alt="Landwind Logo" />
                             CricketCraft
                         </a>
                     </div>
